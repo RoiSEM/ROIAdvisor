0348f173ef21e4a899cd5ac203c5388ded8cc258
Hero-Geo <george.andrews@herounit.io>
2026-03-22 17:36:50 -0700

    login update

--- app/login/login-client.tsx
+++ app/login/login-client.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { supabase } from "@/lib/supabase";
+import { supabase } from "@/lib/supabase-browser";
 import { useSearchParams } from "next/navigation";
 
 export default function LoginClient() {
